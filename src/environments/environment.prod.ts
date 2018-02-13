f0570505f839816b7845eb6eb8171c03c36d4f03
anjiuidev <anjisingavaram@gmail.com>
2018-02-13 13:01:34 +0530

    Angular project first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    MAPBOX_API_KEY: 'pk.eyJ1IjoiYW5qaTQwNyIsImEiOiJjamQwODM1enUyMXo1MzNxb29jNGNpN2tyIn0.rcMq8LcuiPFnh650AabeRA'
 };
